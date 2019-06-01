839650193ce5ae92b20360d9644aba1d6fde0d6d
K.VAMSHIKRISHNA <k.vamshikrishna2025@gmail.com>
2019-06-01 11:41:30 +0530

    Extractor

--- Extractor/SpringBatchdata/src/test/java/com/example/demo/SpringBatchdataApplicationTests.java
+++ Extractor/SpringBatchdata/src/test/java/com/example/demo/SpringBatchdataApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Extractor1ApplicationTests {
+public class SpringBatchdataApplicationTests {
 
 	@Test
 	public void contextLoads() {
